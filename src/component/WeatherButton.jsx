eb9e5cf857d6a27c642400252d7465d9e5e1a059
YewonUm <0816yw@gmail.com>
2025-05-20 13:39:06 +0900

    이미지 경로 수정

--- src/component/WeatherButton.jsx
+++ src/component/WeatherButton.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Button } from 'react-bootstrap';
-
+import gps from '../image/gps.png';
 
 const WeatherButton = ({cities, setCity, selectedCity}) => {
     return (
     <div className="weather-font">
-        <Button variant={selectedCity == '' ? 'secondary' : 'light'} onClick={()=>{setCity('')}}><img width="25" src="src/image/gps.png"/></Button>
+        <Button variant={selectedCity == '' ? 'secondary' : 'light'} onClick={()=>{setCity('')}}><img width="25" src={gps}/></Button>
 
         {cities.map((item, index)=>{
             return <Button variant={selectedCity == item ? 'secondary' : 'light'} key={index} onClick={()=>{setCity(item)}}>
